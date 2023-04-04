5d70dc54bedca320b4c2f2455a1cdd4881fc0f02
RobinBONY <57664687+RobinBONY@users.noreply.github.com>
2023-04-04 11:49:29 +0200

    correcction userPresence dto

--- backend/src/user-presences/dto/user-presence.input.ts
+++ backend/src/user-presences/dto/user-presence.input.ts
@@ -4,15 +4,6 @@ import { Field, InputType } from "@nestjs/graphql";
 //  MUTATION
 //**************************************************//
 
-@InputType()
-export class UpdateMessageForChannelInput {
-    @Field(() => String)
-    id: string;
-
-    @Field(() => Date)
-    disconnectedAt: Date;
-}
-
 //**************************************************//
 //  QUERY
 //**************************************************//
