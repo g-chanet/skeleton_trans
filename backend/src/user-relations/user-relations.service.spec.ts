c13a1352f85820bf4e2b00a3087fab9064016e87
gchanet <gaelchanet@hotmail.com>
2023-04-04 19:58:12 +0200

    Add tests....

--- backend/src/user-relations/user-relations.service.spec.ts
+++ backend/src/user-relations/user-relations.service.spec.ts
@@ -1,17 +1,16 @@
-import { EUserRelationType } from '@prisma/client'
+import { EUserRelationType, UserRelation, User } from '@prisma/client'
 // import { UsersModule } from 'src/users/users.module'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { UserRelationsService } from './user-relations.service'
 import { UsersService } from 'src/users/users.service'
 import { UsersModule } from 'src/users/users.module'
-import { User } from 'src/users/entities/user.entity'
 
 describe(`UserRelationsService`, () => {
   let userRelationsService: UserRelationsService
   let usersService: UsersService
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserRelationsService],
       imports: [PrismaModule, UsersModule],
@@ -22,39 +21,66 @@ describe(`UserRelationsService`, () => {
     usersService = module.get<UsersService>(UsersService)
   })
 
-  it(`should be defined`, () => {
+  it(`Services should be defined`, () => {
     expect(userRelationsService).toBeDefined()
     expect(usersService).toBeDefined()
   })
 
-  describe(`Init user`, () => {
+  describe(`Test d'amitié`, () => {
     let userA: User
     let userB: User
+    let relationA: UserRelation
+    let relationB: UserRelation
 
     beforeAll(async () => {
       userA = await usersService.create({
         email: `userA@test.app`,
         username: `userA`,
       })
-      console.log(`userA`, userA)
       userB = await usersService.create({
         email: `userB@test.app`,
         username: `userB`,
       })
     })
 
-    describe(`UserA demande UserB`, () => {
-      it(`UserA demande UserB en ami`, async () => {
+    describe(`Test de la demande d'ami`, () => {
+      it(`Envoie de la demande`, async () => {
         await userRelationsService.createRequestFriend(userA.id, userB.id)
       })
-      it(`La relation UserA vers UserB est bien de type PendingAccept`, async () => {
-        const relationA = await userRelationsService.findOne(userA.id, userB.id)
+      it(`La relation du UserA vers UserB a bien été créer`, async () => {
+        relationA = await userRelationsService.findOne(userA.id, userB.id)
+        expect(relationA).toBeDefined()
+      })
+      it(`La relation du UserA vers UserB est bien de type PendingAccept`, () => {
         expect(relationA.type).toBe(EUserRelationType.PendingAccept)
       })
-      it(`La relation UserB vers UserA est bien de type WaitingAccept`, async () => {
-        const relationB = await userRelationsService.findOne(userB.id, userA.id)
+      it(`La relation du UserB vers UserA a bien été créer`, async () => {
+        relationB = await userRelationsService.findOne(userB.id, userA.id)
+        expect(relationB).toBeDefined()
+      })
+      it(`La relation du UserB vers UserA est bien de type WaitingAccept`, () => {
         expect(relationB.type).toBe(EUserRelationType.WaitingAccept)
       })
+      it(`Émission d'une erreur car la demande de UserA vers UserB a été renouvelée`, async () => {
+        await expect(
+          userRelationsService.createRequestFriend(userA.id, userB.id),
+        ).rejects.toThrow()
+      })
+    })
+    describe(`Test de l'acceptation d'amitié`, () => {
+      beforeAll(async () => {})
+    })
+    describe(`Test du refus d'amitié`, () => {
+      beforeAll(async () => {})
+    })
+    describe(`Test du bloquage de relation`, () => {
+      beforeAll(async () => {})
+    })
+    describe(`Test du déblocage de relation`, () => {
+      beforeAll(async () => {})
+    })
+    describe(`Test de la suppression d'amitié `, () => {
+      beforeAll(async () => {})
     })
 
     afterAll(async () => {
