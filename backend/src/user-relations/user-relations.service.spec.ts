7b4ac592606be0f45da7c5d773219441dfb96480
kbarbry <kellianbarbry@gmail.com>
2023-04-06 12:53:56 +0200

    Add more testing

--- backend/src/user-relations/user-relations.service.spec.ts
+++ backend/src/user-relations/user-relations.service.spec.ts
@@ -26,72 +26,152 @@ describe(`UserRelationsService`, () => {
     expect(usersService).toBeDefined()
   })
 
-  describe(`Test d'amitié`, () => {
+  describe(`Friendship tests`, () => {
     let userA: User
     let userB: User
     let relationA: UserRelation
     let relationB: UserRelation
+    const userAMail = `userA@test.app`
+    const userBMail = `userB@test.app`
 
-    beforeAll(async () => {
-      userA = await usersService.create({
-        email: `userA@test.app`,
-        username: `userA`,
+    it(`DataBase cleared verification`, async () => {
+      expect(await usersService.findOneByEmail(userAMail)).toBeNull()
+      expect(await usersService.findOneByEmail(userBMail)).toBeNull()
+    })
+    describe(`Request friend tests`, () => {
+      describe(`No relations case`, () => {
+        it(`Create 2 new users`, async () => {
+          userA = await usersService.create({
+            email: userAMail,
+            username: `userA`,
+          })
+          userB = await usersService.create({
+            email: userBMail,
+            username: `userB`,
+          })
+        })
+        it(`A->B Friend request`, async () => {
+          await userRelationsService.createRequestFriend(userA.id, userB.id)
+        })
+        it(`A->B Relation exists`, async () => {
+          relationA = await userRelationsService.findOne(userA.id, userB.id)
+          expect(relationA).toBeDefined()
+        })
+        it(`A->B Relation type is PendingAccept`, () => {
+          expect(relationA.type).toBe(EUserRelationType.PendingAccept)
+        })
+        it(`B->A Relation exists`, async () => {
+          relationB = await userRelationsService.findOne(userB.id, userA.id)
+          expect(relationB).toBeDefined()
+        })
+        it(`B->A Relation type is WaitingAccept`, () => {
+          expect(relationB.type).toBe(EUserRelationType.WaitingAccept)
+        })
+        it(`[Error] A->B Friend request resent`, async () => {
+          await expect(
+            userRelationsService.createRequestFriend(userA.id, userB.id),
+          ).rejects.toThrow()
+        })
+        it(`B->A Friend request`, async () => {
+          await userRelationsService.createRequestFriend(userB.id, userA.id)
+        })
+        it(`A->B Relation type is Friend`, async () => {
+          relationA = await userRelationsService.findOne(userA.id, userB.id)
+          expect(relationA.type).toBe(EUserRelationType.Friend)
+        })
+        it(`B->A Relation type is Friend`, async () => {
+          relationB = await userRelationsService.findOne(userB.id, userA.id)
+          expect(relationB.type).toBe(EUserRelationType.Friend)
+        })
+        it(`[Error] A->B Friend request resent`, async () => {
+          await expect(
+            userRelationsService.createRequestFriend(userA.id, userB.id),
+          ).rejects.toThrow()
+        })
+        it(`[Error] B->A Friend request resent`, async () => {
+          await expect(
+            userRelationsService.createRequestFriend(userB.id, userA.id),
+          ).rejects.toThrow()
+        })
+        it(`Clear DataBase`, async () => {
+          await usersService.delete(userA.id)
+          await usersService.delete(userB.id)
+        })
+        it(`A and B deleted`, async () => {
+          expect(await usersService.findOne(userA.id)).toBeNull()
+          expect(await usersService.findOne(userB.id)).toBeNull()
+        })
       })
-      userB = await usersService.create({
-        email: `userB@test.app`,
-        username: `userB`,
+      describe(`Already friend case`, () => {
+        it(`Create 2 new users`, async () => {
+          userA = await usersService.create({
+            email: userAMail,
+            username: `userA`,
+          })
+          userB = await usersService.create({
+            email: userBMail,
+            username: `userB`,
+          })
+        })
+        it(`User A and B defined`, async () => {
+          expect(await usersService.findOne(userA.id)).toBeDefined()
+          expect(await usersService.findOne(userB.id)).toBeDefined()
+        })
+        it(`A->B Friend request`, async () => {
+          await userRelationsService.createRequestFriend(userA.id, userB.id)
+        })
+        // describe(`Blocked friend case`, () => {})
       })
     })
 
-    describe(`Test de la demande d'ami`, () => {
-      it(`Envoie de la demande`, async () => {
-        await userRelationsService.createRequestFriend(userA.id, userB.id)
-      })
-      it(`La relation du UserA vers UserB a bien été créée`, async () => {
-        relationA = await userRelationsService.findOne(userA.id, userB.id)
-        expect(relationA).toBeDefined()
-      })
-      it(`La relation du UserA vers UserB est bien de type PendingAccept`, () => {
-        expect(relationA.type).toBe(EUserRelationType.PendingAccept)
-      })
-      it(`La relation du UserB vers UserA a bien été créer`, async () => {
-        relationB = await userRelationsService.findOne(userB.id, userA.id)
-        expect(relationB).toBeDefined()
-      })
-      it(`La relation du UserB vers UserA est bien de type WaitingAccept`, () => {
-        expect(relationB.type).toBe(EUserRelationType.WaitingAccept)
-      })
-      it(`Émission d'une erreur car la demande de UserA vers UserB a été renouvelée`, async () => {
-        await expect(
-          userRelationsService.createRequestFriend(userA.id, userB.id),
-        ).rejects.toThrow()
-      })
-    })
-    describe(`Test de l'acceptation d'amitié`, () => {
-      beforeAll(async () => {
-        //TODO
-      })
-    })
-    describe(`Test du refus d'amitié`, () => {
-      beforeAll(async () => {
-        //TODO
-      })
-    })
-    describe(`Test du bloquage de relation`, () => {
-      beforeAll(async () => {
-        //TODO
-      })
-    })
-    describe(`Test du déblocage de relation`, () => {
-      beforeAll(async () => {
-        //TODO
-      })
-    })
-    describe(`Test de la suppression d'amitié `, () => {
-      beforeAll(async () => {
-        //TODO
-      })
-    })
+    // describe(`Test du refus d'amitié`, () => {
+    //   beforeAll(async () => {
+    //     userA = await usersService.create({
+    //       email: `userA@test.app`,
+    //       username: `userA`,
+    //     })
+    //     userB = await usersService.create({
+    //       email: `userB@test.app`,
+    //       username: `userB`,
+    //     })
+    //   })
+    // })
+    // describe(`Test du bloquage de relation`, () => {
+    //   beforeAll(async () => {
+    //     userA = await usersService.create({
+    //       email: `userA@test.app`,
+    //       username: `userA`,
+    //     })
+    //     userB = await usersService.create({
+    //       email: `userB@test.app`,
+    //       username: `userB`,
+    //     })
+    //   })
+    // })
+    // describe(`Test du déblocage de relation`, () => {
+    //   beforeAll(async () => {
+    //     userA = await usersService.create({
+    //       email: `userA@test.app`,
+    //       username: `userA`,
+    //     })
+    //     userB = await usersService.create({
+    //       email: `userB@test.app`,
+    //       username: `userB`,
+    //     })
+    //   })
+    // })
+    // describe(`Test de la suppression d'amitié `, () => {
+    //   beforeAll(async () => {
+    //     userA = await usersService.create({
+    //       email: `userA@test.app`,
+    //       username: `userA`,
+    //     })
+    //     userB = await usersService.create({
+    //       email: `userB@test.app`,
+    //       username: `userB`,
+    //     })
+    //   })
+    // })
 
     afterAll(async () => {
       await usersService.delete(userA.id)
