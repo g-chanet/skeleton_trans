90c9dfc0565219bca2fa4e6fb3f5d8d37d9ce642
root <root@DESKTOP-KEM7SGA.localdomain>
2023-04-12 11:37:38 +0200

    Added first mock for discord auth strategy

--- backend/src/auth/strategies/discord.strategy.ts
+++ backend/src/auth/strategies/discord.strategy.ts
@@ -1,5 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-discord';
+import { AuthService } from '../auth.service';
+
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 
 @Injectable()
-export class DiscordStrategy extends PassportStrategy(null) {}
+export class DiscordStrategy extends PassportStrategy(Strategy, 'discord') {
+  constructor(private readonly authService: AuthService) {
+    super({
+      clientID: DISCORD_CLIENT_ID,
+      clientSecret: DISCORD_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/discord/callback',
+      scope: ['identify', 'email'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ) {
+    return;
+  }
+}
+
+//npm install passport-discord
