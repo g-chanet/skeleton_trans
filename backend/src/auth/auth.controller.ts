8a001e324ab7a6da6e8a0c421e1a83cfbd89f5d1
VictorDé <vducoulo@student.42lyon.fr>
2023-06-13 17:51:51 +0200

    added discord ouath redirect

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,25 @@
 import { Controller, Request, UseGuards, Get } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { GoogleOAuthGuard } from './guards/google-auth.guard'
+import { DiscordOAuthGuard } from './guards/discord-auth.guard'
 
 @Controller(`auth`)
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Get(`google`)
   @UseGuards(GoogleOAuthGuard)
-  async googleAuth(@Request() req) {}
+  async googleAuth(@Request() req) { }
 
   @Get(`google-redirect`)
   @UseGuards(GoogleOAuthGuard)
-  googleAuthRedirect(@Request() req) {}
+  googleAuthRedirect(@Request() req) { }
+
+  @Get(`discord`)
+  @UseGuards(DiscordOAuthGuard)
+  async discordAuth(@Request() req) { }
+
+  @Get(`discord-redirect`)
+  @UseGuards(DiscordOAuthGuard)
+  discordAuthRedirect(@Request() req) { }
 }
--- backend/src/auth/strategies/discord.strategy.ts
+++ backend/src/auth/strategies/discord.strategy.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-discord';
+import { Strategy } from 'passport-discord';
+import { VerifyCallback } from 'passport-discord-oauth2'
+import { UsersService } from 'src/users/users.service'
 import { AuthService } from '../auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 
 @Injectable()
 export class DiscordStrategy extends PassportStrategy(Strategy, 'discord') {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private prisma: PrismaService
+    ) {
     super({
       clientID: DISCORD_CLIENT_ID,
       clientSecret: DISCORD_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/discord/callback',
+      callbackURL: 'http://localhost:3000/auth/discord-redirect',
       scope: ['identify', 'email'],
+      passReqToCallback: true,
     });
   }
 
   async validate(
+    req,
     accessToken: string,
     refreshToken: string,
     profile: any,
+    email: string,
     done: VerifyCallback,
   ) {
-    return;
+    console.log(profile, accessToken)
   }
 }
-
-//npm install passport-discord
