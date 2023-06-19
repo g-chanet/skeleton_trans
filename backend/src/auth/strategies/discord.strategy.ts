036dd6c45afaf2324a90572d0c4abddf7f33a657
VictorDé <vducoulo@student.42lyon.fr>
2023-06-19 15:39:08 +0200

    update

--- backend/src/auth/strategies/discord.strategy.ts
+++ backend/src/auth/strategies/discord.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
-import { Strategy, VerifyCallback } from 'passport-discord-oauth2'
+import { Strategy } from 'passport-discord'
+import { VerifyCallback } from 'passport-discord-oauth2'
 import { AuthService } from '../auth.service'
 
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID
