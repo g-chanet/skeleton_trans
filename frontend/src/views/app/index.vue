381198a7068839bc84cadb2074c924f7e17ef85e
root <root@DESKTOP-KEM7SGA.localdomain>
2023-06-21 12:17:46 +0200

    fixed blur layout on base template

--- frontend/src/views/app/index.vue
+++ frontend/src/views/app/index.vue
@@ -4,6 +4,7 @@
       <NavBar />
     </el-aside>
     <el-container class="center">
+      <div class="center-background panel-blur" />
 
       <router-view class="center-main" />
     </el-container>
@@ -16,20 +17,23 @@ import NavBar from './navBar/index.vue'
 
 <style scoped lang="sass">
 .container-app
-  position: relative
-  height: 95%
-  width: 95%
-  background: #b000ff
+  height: 100%
+  padding: 20px 0px 20px 0px 
   border-radius: 20px
   // background: linear-gradient(6deg, rgba(233,172,99,1) 0%, rgba(221,146,111,1) 100%)
   .aside
     width: 80px
   .center
     position: relative
-    background: #c700b5
-    backdrop-filter: blur(20px)
-    border-radius: 20px
-  .center-main
+    margin-right: 20px
+    .center-background
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      border-radius: 20px
+    .center-main
       position: relative
       padding: 20px
       
