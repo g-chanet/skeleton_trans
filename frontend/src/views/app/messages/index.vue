b44272f559e28eeb319d3c6a2f5b78ea2076a228
root <root@DESKTOP-KEM7SGA.localdomain>
2023-06-21 17:31:49 +0200

    Finished templates for dashboard, messages, leaderboard and almost finished profile

--- frontend/src/views/app/messages/index.vue
+++ frontend/src/views/app/messages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="messages-container">
-    <div class="convesations-container">
+    <div class="conversations-container">
       <h1>Discussions</h1>
-      <div class="convesations-list-container">
+      <div class="conversations-list-container">
         <div>partie1</div>
         <div>partie2</div>
         <div>partie3</div>
@@ -36,16 +36,16 @@
   justify-content: space-between
   border-radius:20px
 
-.convesations-container
+.conversations-container
   background: red
   border-radius: 20px
   width: 100%
   z-index: 1
 
-.convesations-list-container
+.conversations-list-container
   display: flex
   flex-direction: column
-  gap:20px
+  gap: 20px
   flex:1
   overflow-y: scroll
   div
@@ -61,35 +61,35 @@
   width: 100%
   background: green
 
-.messageBlue
-  position: relative
-  marginLeft: 20px
-  marginBottom: 10px
-  padding: 10px
-  backgroundColor: #A8DDFD
-  width: 60%
-  textAlign: left
-  border: 1px solid #97C6E3
-  border-radius: 10px
-    &:after
-      content: ''
-      position: absolute
-      width: 0
-      height: 0
-      borderTop: 15px solid #A8DDFD
-      borderLeft: 15px solid transparent
-      borderRight: 15px solid transparent
-      top: 0
-      left: -15px
-    &:before: 
-      content: ''
-      position: absolute
-      width: 0
-      height: 0
-      borderTop: 17px solid #97C6E3
-      borderLeft: 16px solid transparent
-      borderRight: 16px solid transparent
-      top: -1px
-      left: -17px
+// .messageBlue
+//   position: relative
+//   margin-left: 20px
+//   margin-bottom: 10px
+//   padding: 10px
+//   background-color: #A8DDFD
+//   width: 60%
+//   text-align: left
+//   border: 1px solid #97C6E3
+//   border-radius: 10px
+//     &:after
+//       content: ''
+//       position: absolute
+//       width: 0
+//       height: 0
+//       borderTop: 15px solid #A8DDFD
+//       borderLeft: 15px solid transparent
+//       borderRight: 15px solid transparent
+//       top: 0
+//       left: -15px
+//     &:before: 
+//       content: ''
+//       position: absolute
+//       width: 0
+//       height: 0
+//       borderTop: 17px solid #97C6E3
+//       borderLeft: 16px solid transparent
+//       borderRight: 16px solid transparent
+//       top: -1px
+//       left: -17px
 </style>
 
